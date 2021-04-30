27754a338687af62db76bce2273d109bd52056c7
Darryl Parks <djparks@usa.net>
2021-04-30 15:35:38 -0500

    Start Code

---
+++ NOTES.md
@@ -0,0 +1,26 @@
+# Notes and Help for React
+
+## Command to create and start
+
+* `npx create-react-app demo-react`
+* `cd demo-react`
+* `npm start`
+
+* Note: don't commit anything stored in node_modules into Repository. 
+  * Do `npm install`
+
+## Useful VS Code Plug-ins
+
+* Auto Import (Setup js,jsx extensions)
+* Auto Rename Tag
+* Bracket Pair Colorizer 2.
+* ESLint
+* JavaScript Debugger
+* Material Icon Theme
+* NPM Intellisense
+* Path Intellisense
+* Prettier - Code formatter
+* settings.json:
+    * files.exclude: ".idea"
+    * "files.autoSave": "afterDelay"
+    * "editor.autoClosingQuotes": "always"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
+// TODO DJP StrictMode is new... will cause lots of warnings...
+
+const rootEl = document.getElementById('root');
+
+    /*
+      Not a big deal, but this causes smoother instant re-render w/o flash.
+     */
+    function render() {
+        ReactDOM.render(<App />, rootEl);
+    }
+
+  if (module.hot) {
+      module.hot.accept('./app/layout/App.jsx', function () {
+          setTimeout(render);
+      })
+  }
+
+  render();
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+    rootEl
+  /!*document.getElementById('root')*!/
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
